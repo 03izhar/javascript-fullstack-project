c73e0231405d005243e4a23f8921948d8041b6fb
un-rahul <un-rahul@indicchain.com>
2023-11-21 10:42:43 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from "cors"
-import bodyParser from "body-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,7 +12,14 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+
+// Routes Declaration 
+app.use("/api/v1/users", userRouter);
 
 
 export { app }
